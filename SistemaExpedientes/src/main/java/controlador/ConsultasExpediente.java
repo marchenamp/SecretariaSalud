bed092c35060db4b05e00c61c8da7ced12747569
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2024-04-29 17:42:42 -0700

    Creación de proyecto "Sistema Expediente"

---
+++ SistemaExpedientes/src/main/java/controlador/ConsultasExpediente.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author march
+ */
+public class ConsultasExpediente extends Conexion{
+
+    public ConsultasExpediente() {
+    }
+    
+    public boolean registrarExpediente(String tipoSangre, String estatura, float peso, String alergias, String frecuenciaCardiaca, String padecimientosPersonales, String antecedentesHereditarios, String nombreContactoEmergencia, String telefonoContactoEmergencia, int idPaciente) {
+        PreparedStatement pst = null;
+        try {
+            String consulta = "INSERT INTO expedientes (tipo_sangre, estatura, peso, alergias, frecuencia_cardiaca, padecimientos_personales, antecedentes_hereditarios, nombre_contacto_emergencia, telefono_contacto_emergencia, id_paciente) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            pst = getConexion().prepareStatement(consulta);
+            pst.setString(1, tipoSangre);
+            pst.setString(2, estatura);
+            pst.setFloat(3, peso);
+            pst.setString(4, alergias);
+            pst.setString(5, frecuenciaCardiaca);
+            pst.setString(6, padecimientosPersonales);
+            pst.setString(7, antecedentesHereditarios);
+            pst.setString(8, nombreContactoEmergencia);
+            pst.setString(9, telefonoContactoEmergencia);
+            pst.setInt(10, idPaciente);
+
+            if (pst.executeUpdate() == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            System.err.println("Error en: " + e);
+        } finally {
+//            try {
+//                if (getConexion() != null) {
+//                    getConexion().close();
+//                }
+//                if (pst != null) {
+//                    pst.close();
+//                }
+//            } catch (Exception e) {
+//                System.err.println("Error en: " + e);
+//            }
+        }
+        return false;
+    }
+}
