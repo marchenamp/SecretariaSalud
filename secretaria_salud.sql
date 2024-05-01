64fc9d365d6101cf7611918a30c29aeb46c92ac5
DESKTOP-4RU6P74\march <misael.marchena233418@potros.itson.edu.mx>
2024-05-01 12:28:32 -0700

    Base de datos actualizada con médico

--- secretaria_salud.sql
+++ secretaria_salud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-04-2024 a las 01:25:51
+-- Tiempo de generación: 01-05-2024 a las 21:27:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -41,6 +41,41 @@ CREATE TABLE `expedientes` (
   `id_paciente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `expedientes`
+--
+
+INSERT INTO `expedientes` (`id_expediente`, `tipo_sangre`, `estatura`, `peso`, `alergias`, `frecuencia_cardiaca`, `padecimientos_personales`, `antecedentes_hereditarios`, `nombre_contacto_emergencia`, `telefono_contacto_emergencia`, `id_paciente`) VALUES
+(4, 'A-', '1.90', 75, 'no', '98', 'Hernia hiatal', 'Abuela con cancer de mama', 'Magda Ramirez Escalante', '6688293570', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `id_medico` int(11) NOT NULL,
+  `nombres` varchar(300) NOT NULL,
+  `apellidopaterno` varchar(150) NOT NULL,
+  `apellidomaterno` varchar(150) NOT NULL,
+  `correo` varchar(300) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `genero` varchar(50) NOT NULL,
+  `cedula_profesional` varchar(8) NOT NULL,
+  `especialidad_medica` varchar(200) NOT NULL,
+  `lugar_trabajo_actual` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`id_medico`, `nombres`, `apellidopaterno`, `apellidomaterno`, `correo`, `password`, `fecha_nacimiento`, `telefono`, `genero`, `cedula_profesional`, `especialidad_medica`, `lugar_trabajo_actual`) VALUES
+(1, 'Alfredo', 'Lopez', 'Lugardo', 'alfredolopez@gmail.com', 'Alfredo123', '1990-08-15', '6442873971', 'MASCULINO', '17652833', 'Gastroenterologia', 'ISSSTESON');
+
 -- --------------------------------------------------------
 
 --
@@ -61,6 +96,14 @@ CREATE TABLE `pacientes` (
   `id_tutor` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `pacientes`
+--
+
+INSERT INTO `pacientes` (`id_paciente`, `nombres`, `apellidopaterno`, `apellidomaterno`, `correo`, `password`, `fecha_nacimiento`, `telefono`, `estado_civil`, `genero`, `id_tutor`) VALUES
+(7, 'Misael', 'Marchena', 'Perez', 'misaelmarchenaperez@gmail.com', 'jordan02MAR', '2003-05-27', '6442322888', 'SOLTERO', 'MASCULINO', NULL),
+(8, 'Amy Marsella', 'Marchena', 'Perez', 'amy@gmail.com', 'jordan02MAR', '2011-07-27', '6441324532', 'SOLTERO', 'FEMENINO', 11);
+
 -- --------------------------------------------------------
 
 --
@@ -79,6 +122,13 @@ CREATE TABLE `tutores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `tutores`
+--
+
+INSERT INTO `tutores` (`id_tutor`, `nombres`, `apellidopaterno`, `apellidomaterno`, `fecha_nacimiento`, `telefono`, `genero`, `parentesco`) VALUES
+(11, 'Marcela Arely', 'Perez', 'Valenzuela', '1980-10-21', '6441523557', 'FEMENINO', 'Madre');
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -88,6 +138,12 @@ CREATE TABLE `tutores` (
 ALTER TABLE `expedientes`
   ADD PRIMARY KEY (`id_expediente`),
   ADD KEY `id_paciente` (`id_paciente`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`id_medico`);
 
 --
 -- Indices de la tabla `pacientes`
@@ -110,19 +166,25 @@ ALTER TABLE `tutores`
 -- AUTO_INCREMENT de la tabla `expedientes`
 --
 ALTER TABLE `expedientes`
-  MODIFY `id_expediente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_expediente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_paciente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `tutores`
 --
 ALTER TABLE `tutores`
-  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_tutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
